8d8d0070c95823bc6c55ac05069c992b41fc2da8
Brian Ray Xu <33497224+brianrayxu@users.noreply.github.com>
2021-03-30 21:12:13 -0400

    Added Maps functionality

--- App.js
+++ App.js
@@ -1,23 +1,46 @@
 //import 'react-native-gesture-handler';
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import MapView from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { RNCamera, FaceDetector } from 'react-native-camera';
 
+
+const styles = StyleSheet.create({
+ container: {
+   ...StyleSheet.absoluteFillObject,
+   height: 400,
+   width: 400,
+   justifyContent: 'flex-end',
+   alignItems: 'center',
+ },
+ map: {
+   ...StyleSheet.absoluteFillObject,
+ },
+});
 
 function DetailsScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-         <MapView
-            initialRegion={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            }}
-          />
+      <MapView
+             provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+             style={styles.map}
+             region={{
+               latitude: 37.78825,
+               longitude: -122.4324,
+               latitudeDelta: 0.015,
+               longitudeDelta: 0.0121,
+             }}
+           >
+           </MapView>
+    </View>
+  );
+}
+function CameraScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Camera Screen</Text>
     </View>
   );
 }
@@ -29,6 +52,10 @@ function HomeScreen({ navigation }) {
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
+      />
+      <Button
+          title="Go to Camera"
+          onPress={() => navigation.navigate('Camera')}
       />
     </View>
   );
@@ -42,6 +69,7 @@ function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Camera" component={CameraScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
